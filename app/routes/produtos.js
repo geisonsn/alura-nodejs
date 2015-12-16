cbaca1eb9a293088ad8fb523fe36659dd2022740
= <geison.sn@gmail.com>
2015-12-16 14:50:49 -0400

    Refatoração no código de conexão e acesso ao banco de dados

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,14 +3,14 @@ module.exports = function(app) {
     app.get("/produtos", function(req, res) {
         
         var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco;
+        var produtosBanco = app.infra.produtosBanco(connection);
 
         /*connection.query("select * from livros", function(err, result) {
             //res.send(result);
             res.render("produtos/lista", {lista : result});
         });*/
 
-        produtosBanco.lista(connection, function(err, result) {
+        produtosBanco.lista(function(err, result) {
             res.render("produtos/lista", {lista:result});
         });
 
