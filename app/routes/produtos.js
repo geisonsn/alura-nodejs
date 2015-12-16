2c705db1c0c53f751e2aa12d7087eb7ac5198b94
= <geison.sn@gmail.com>
2015-12-16 14:11:46 -0400

    Refatoração para isolar as consultas de produtos em um arquivo separado

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,10 +3,15 @@ module.exports = function(app) {
     app.get("/produtos", function(req, res) {
         
         var connection = app.infra.connectionFactory();
+        var produtosBanco = app.infra.produtosBanco;
 
-        connection.query("select * from livros", function(err, result) {
+        /*connection.query("select * from livros", function(err, result) {
             //res.send(result);
             res.render("produtos/lista", {lista : result});
+        });*/
+
+        produtosBanco.lista(connection, function(err, result) {
+            res.render("produtos/lista", {lista:result});
         });
 
         connection.end();
