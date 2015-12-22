0f8bbdef4b45ecb334e06ae063dd81397b225cc2
= <geison.sn@gmail.com>
2015-12-22 09:31:54 -0400

    Implementação da funcionalidade de inclusão de livros

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,20 +1,29 @@
 
 module.exports = function(app) {
-    app.get("/produtos", function(req, res) {
-        
+
+    var listaProdutos = function(req, res) {
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
-
-        /*connection.query("select * from livros", function(err, result) {
-            //res.send(result);
-            res.render("produtos/lista", {lista : result});
-        });*/
-
         produtosDAO.lista(function(err, result) {
             res.render("produtos/lista", {lista:result});
         });
-
         connection.end();
+    }; 
 
+    app.get("/produtos", listaProdutos);
+
+    app.get("/produtos/form", function(req, res) {
+       res.render("produtos/form");
+    });
+
+    app.post("/produtos", function(req, res) {
+        
+        var produto = req.body;
+        
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function(erros, resultado) {
+            res.redirect("/produtos");
+        });
     });
 }
