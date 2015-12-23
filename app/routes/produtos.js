79925bff881bc2fb56b34ed47473188d5fb958db
= <geison.sn@gmail.com>
2015-12-23 11:54:33 -0400

    Capítulo 5: validação de formulário de formulário com express-validator, retorno adequado do código http

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -29,15 +29,31 @@ module.exports = function(app) {
     */
 
     app.get("/produtos/form", function(req, res) {
-       res.render("produtos/form");
+       res.render("produtos/form", {errosValidacao : {}, produto : {}});
     });
 
     app.post("/produtos", function(req, res) {
         
         var produto = req.body;
 
-        console.log(produto);
-        
+        req.assert("titulo", "Título é obrigatório").notEmpty();
+        req.assert("preco", "Formato inválido").isFloat();
+
+        var erros = req.validationErrors();
+        if (erros) {
+           // res.render("produtos/form", {errosValidacao : erros, produto : produto});
+            res.format({
+                html : function() {
+                    res.status(400).render("produtos/form", {errosValidacao:erros, produto:produto});
+                }, 
+                json : function() {
+                    res.status(400).json(erros);
+                }
+            });
+
+            return;
+        }
+
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
         produtosDAO.salva(produto, function(erros, resultado) {
